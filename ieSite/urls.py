0e7f653be8b8f211abc131bfa317a845680990ec
Adit Biswas <aditbiswas1@gmail.com>
2014-10-28 11:36:58 +0530

    changed homepage to events page

--- ieSite/urls.py
+++ ieSite/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    url(r'^$', 'recruitments.views.home', name='home'),
+    url(r'^$', 'eventRegistrations.views.eventsHome', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^bootcamp/', include('bootcamp.urls')),
     url(r'^sig/', include('SIG.urls')),
