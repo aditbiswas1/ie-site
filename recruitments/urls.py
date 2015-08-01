856baca9e26580c47ecb719b10f66e4e9ed0fcc5
abhijithanilkumar <abhijithanilkumar@live.com>
2015-08-01 19:05:33 +0530

    Concurrency Problem for Resume Evaluation Fixed!

--- recruitments/urls.py
+++ recruitments/urls.py
@@ -8,4 +8,7 @@ urlpatterns = patterns('',
 	url(r'^submit$', 'recruitments.views.submit_resume', name='submission'),
 	url(r'^evaluate$', 'recruitments.views.evaluate_view', name='evaluation_main'),
 	url(r'^evaluate/(?P<resume_id>\w{0,50})$', 'recruitments.views.evaluate_resume', name='evaluation'),
+    url(r'^pi$', 'recruitments.views.get_pi', name='pi_list'),
+    url(r'^gd$', 'recruitments.views.get_gd', name='gd_list'),
+    url(r'^evaluate_pigd/(?P<resume_id>\w{0,50})$', 'recruitments.views.evaluate_pigd', name='evaluate_pigd'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recruitments/views.py
+++ recruitments/views.py
@@ -27,7 +27,15 @@ def submit_resume(request):
 
 @login_required
 def evaluate_view(request):
-	return render(request, 'flatpages/resumes_to_be_evaluated.html', {"resumes":models.Resume.objects.exclude(qualified_for_round=0).exclude(qualified_for_round=5).order_by('name','current_round')})
+	return render(request, 'flatpages/resumes_to_be_evaluated.html', {"resumes":models.Resume.objects.filter(qualified_for_round=1).order_by('name','current_round')})
+
+@login_required
+def get_pi(request):
+	return render(request, 'flatpages/pis_to_be_evaluated.html', {"resumes":models.Resume.objects.filter(qualified_for_round=2).order_by('name','current_round')})
+
+@login_required
+def get_gd(request):
+	return render(request, 'flatpages/gds_to_be_evaluated.html', {"resumes":models.Resume.objects.filter(qualified_for_round=3).order_by('name','current_round')})
 
 @login_required
 def evaluate_resume(request,resume_id):
@@ -60,13 +68,13 @@ def evaluate_resume(request,resume_id):
 				new_evaluation.save()
 				print models.ResumeEvaluation.objects.filter(resume=current_resume,qualified=True)
 				#for resume round, pass the candidate only if 2 people pass the candidate, but for other rounds, one is sufficient
-				if (current_resume.qualified_for_round == 1 and models.ResumeEvaluation.objects.filter(resume=current_resume,qualified=True).__len__() == 2) or (current_resume.qualified_for_round > 1 and current_resume.qualified_for_round < 5):
+				if (current_resume.qualified_for_round == 1 and models.ResumeEvaluation.objects.filter(resume=current_resume,qualified=True).__len__() == 2) or (current_resume.qualified_for_round > 1 and current_resume.qualified_for_round == 1):
 					current_resume.qualified_for_round += 1
 			else:
 				new_evaluation.qualified = False
 				new_evaluation.save()
 				#for resume round, fail the candidate only if 2 people fail the candidate, but for other rounds, one is sufficient
-				if (current_resume.qualified_for_round == 1 and models.ResumeEvaluation.objects.filter(resume=current_resume).exclude(qualified=True).__len__() == 2) or (current_resume.qualified_for_round > 1 and current_resume.qualified_for_round < 5):
+				if (current_resume.qualified_for_round == 1 and models.ResumeEvaluation.objects.filter(resume=current_resume).exclude(qualified=True).__len__() == 2) or (current_resume.qualified_for_round > 1 and current_resume.qualified_for_round == 1):
 					current_resume.qualified_for_round = 0
 
 			current_resume.current_round = rounds[current_resume.qualified_for_round]
@@ -75,6 +83,49 @@ def evaluate_resume(request,resume_id):
 			current_resume.save()
 			return render_to_response('flatpages/evaluation_success.html')
 	return render_to_response('flatpages/evaluation_form_layout.html',{'evaluationForm':form,'resume':current_resume},context_instance=RequestContext(request))
+
+@login_required
+def evaluate_pigd(request,resume_id):
+	rounds = ['Not Qualified','Resume Evaluation Started','Personal Interview','Group Discussion','Final Interview','Selected']
+	
+	resume_id = request.GET['resume_id']
+	#in case the url has a trailing '/', in which case the resume_id will contain the slash as well
+	try:
+		int(resume_id)
+	except ValueError:
+		resume_id = resume_id[:-1]
+	
+	current_resume = models.Resume.objects.get(id=resume_id)
+	
+	if request.method == 'GET':
+		form = forms.EvaluateResumeForm()
+	else:
+		form = forms.EvaluateResumeForm(request.POST)
+		
+		if form.is_valid():
+			new_evaluation = form.save(commit=False)
+			new_evaluation.resume = current_resume
+			new_evaluation.score = request.POST['score']
+			new_evaluation.comments = request.POST['comments']
+			new_evaluation.informal_comments = request.POST['informal_comments']
+			new_evaluation.evaluated_by = request.user
+			new_evaluation.name = current_resume.name
+			if 'qualified' in request.POST:
+				new_evaluation.qualified = True
+				new_evaluation.save()
+				print models.ResumeEvaluation.objects.filter(resume=current_resume,qualified=True)
+				current_resume.qualified_for_round += 1
+			else:
+				new_evaluation.qualified = False
+				new_evaluation.save()
+				current_resume.qualified_for_round = 0
+
+			current_resume.current_round = rounds[current_resume.qualified_for_round]
+			new_evaluation.current_round = current_resume.current_round
+			new_evaluation.save()
+			current_resume.save()
+			return render_to_response('flatpages/evaluation_success.html')
+	return render_to_response('flatpages/evaluation_form_layout.html',{'evaluationForm':form,'resume':current_resume,'pigd':True},context_instance=RequestContext(request))
 	
 def results_view(request):
 	result_list = sets.Set()
